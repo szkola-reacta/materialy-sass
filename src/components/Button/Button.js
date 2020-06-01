317dc0d8006fb0efd43c89a619a2d0a7aa94de37
patrykomiotek <patryk.omiotek@gmail.com>
2020-06-01 11:37:56 +0200

    cleanup

---
+++ src/components/Button/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Button() {
+	return(
+		<div>
+			<button className="button-blue">Name</button>
+		</div>
+	);
+}
+
+export default Button;
---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Button from '../Button';
+
+function Menu() {
+	return(
+  	<div>
+    	Menu
+      <Button />
+    </div>
+  );
+}
+
+export default Menu;
